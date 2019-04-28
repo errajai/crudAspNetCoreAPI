897474c2f1a9c570667fe51a08a031f0d7725d43
errajai <errajai.m@gmail.com>
2019-04-28 17:13:52 +0100

    First Commit

---
+++ CRUDAngular6AndASPNetCoreAPI/Conrollers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDAngular6AndASPNetCoreAPI.Conrollers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [Produces("Application/json")]
+        [HttpGet("findall")]
+        public async Task<IActionResult> findAll()
+        {
+            try
+            {
+
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
